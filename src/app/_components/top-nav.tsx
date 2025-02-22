e8c368fd2b038d3f6a4b2dee9078abf79bd4fa19
“Tymon” <tymonrybak@outlook.com>
2025-02-22 14:33:50 +0000

    temporarily remove auth navbar

--- src/app/_components/top-nav.tsx
+++ src/app/_components/top-nav.tsx
@@ -1,15 +1,7 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-
 export function TopNav() {
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
-      <div>Gallery</div>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+      <div>This is the top navigation</div>
     </nav>
   );
 }
