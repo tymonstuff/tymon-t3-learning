f47b5e175f5035332438b2f9b4d5100a065adbdf
“Tymon” <tymonrybak@outlook.com>
2025-02-22 14:34:47 +0000

    tournament schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -2,13 +2,7 @@
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
 import { sql } from "drizzle-orm";
-import {
-  index,
-  integer,
-  pgTableCreator,
-  timestamp,
-  varchar,
-} from "drizzle-orm/pg-core";
+import { integer, pgTableCreator, timestamp, uuid } from "drizzle-orm/pg-core";
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
@@ -16,22 +10,13 @@ import {
  *
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
-export const createTable = pgTableCreator((name) => `tymon-gallery_${name}`);
+export const createTable = pgTableCreator((name) => `tymon-perudo${name}`);
 
-export const images = createTable(
-  "image",
-  {
-    id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
-    name: varchar("name", { length: 256 }).notNull(),
-    url: varchar("url", { length: 1024 }).notNull(),
-    createdAt: timestamp("created_at", { withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-      () => new Date(),
-    ),
-  },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  }),
-);
+export const tournaments = createTable("tournaments", {
+  id: uuid("tournamentId").primaryKey(),
+  rounds: integer("rounds").notNull(),
+  tables: integer("tables").notNull(),
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+});
